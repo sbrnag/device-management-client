482fd57f95f05f1da8f23ef001b6617a117bf917
sbrnag <nagendra.sbr@gmail.com>
2021-01-31 12:35:01 +0530

    added material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1>reactapp</h1> 
+    <Header />
   );
 }
 
